3f8d2c0b4f300020e29cf8babf5ba300074f82b4
Greg Leonard <45019882+greg-el@users.noreply.github.com>
2023-05-16 11:47:13 +0100

    Fix docusaurus config after rebase

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,58 +32,36 @@ module.exports = {
     require.resolve('./snowplow.js'),
     require.resolve('./google.js'),
   ],
-  return {
-    staticDirectories: ['static'],
-    scripts: [
-      {
-        src: '/scripts/init.js',
-        async: true,
-      },
-    ],
-    title: 'Snowplow Documentation',
-    tagline:
-      'Build, deploy, and scale your next data creation project using Snowplow.',
-    url: 'https://docs.snowplow.io',
-    baseUrl: '/',
-    // reset this back to throw, set to warn so that site builds
-    onBrokenLinks: 'warn',
-    onBrokenMarkdownLinks: 'throw',
-    favicon: 'img/favicon.ico',
-    trailingSlash: true,
-    organizationName: 'snowplow',
-    projectName: 'documentation',
-    deploymentBranch: 'main',
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en'],
-    },
 
   markdown: {
     mermaid: true,
   },
-    plugins: [
-      async function webpackConfig(context, options) {
-        return {
-          name: 'webpackConfig',
-          configureWebpack(config, isServer, utils) {
-            return {
-              plugins: [
-                new webpack.ProvidePlugin({
-                  process: require.resolve('process/browser'),
-                  Buffer: require.resolve('buffer'),
-                }),
-              ],
-            }
-          },
-        }
-      },
-    ],
 
-    clientModules: [
-      require.resolve('./cookieConsent.js'),
-      require.resolve('./snowplow.js'),
-      require.resolve('./google.js'),
-    ],
+  staticDirectories: ['static'],
+  scripts: [
+    {
+      src: '/scripts/init.js',
+      async: true,
+    },
+  ],
+
+  plugins: [
+    async function webpackConfig(context, options) {
+      return {
+        name: 'webpackConfig',
+        configureWebpack(config, isServer, utils) {
+          return {
+            plugins: [
+              new webpack.ProvidePlugin({
+                process: require.resolve('process/browser'),
+                Buffer: require.resolve('buffer'),
+              }),
+            ],
+          }
+        },
+      }
+    },
+  ],
 
   themes: [
     '@saucelabs/theme-github-codeblock',
@@ -154,90 +132,76 @@ module.exports = {
             label: 'Discourse',
             position: 'left',
           },
-          items: [
-            {
-              type: 'doc',
-              docId: 'introduction',
-              position: 'left',
-              className: 'nav-link__first',
-              label: 'Docs',
-            },
-            {
-              href: 'https://discourse.snowplow.io',
-              label: 'Discourse',
-              position: 'left',
-            },
-            {
-              href: 'https://github.com/snowplow/',
-              label: 'GitHub',
-              position: 'left',
-            },
-            {
-              to: 'https://try.snowplowanalytics.com/?utm_content=hero-cta&utm_campaign=snowplow-docs',
-              label: 'Try Snowplow',
-              className: 'snwpl-nav-button',
-              position: 'right',
-            },
-            {
-              to: 'https://go.snowplowanalytics.com/l/571483/2021-02-19/3sn5nml',
-              label: 'Book a demo',
-              className: 'snwpl-nav-button',
-              position: 'right',
-            },
-            {
-              type: 'custom-liveSnippetNavbarItem',
-              position: 'right',
-            },
-          ],
-        },
-        footer: {
-          style: 'dark',
-          links: [
-            {
-              label: 'Change cookie preferences',
-              href: '/cookie-preferences',
-            },
-            {
-              label: 'Terms and conditions',
-              href: '/terms-and-conditions',
-            },
-          ],
-          copyright: `Copyright © ${new Date().getFullYear()} Snowplow Analytics Ltd. Built with Docusaurus.`,
-        },
-        prism: {
-          theme: require('prism-react-renderer/themes/shadesOfPurple'),
-          // Docusaurus comes with a subset of commonly used languages -https://github.com/FormidableLabs/prism-react-renderer/blob/master/src/vendor/prism/includeLangs.js.
-          // To add syntax highlighting for additional Prism supported languages, add reference from https://prismjs.com/#supported-languages.
-          // NOTE: do a `yarn build` to ensure that it does build properly
-          additionalLanguages: [
-            'arduino',
-            'csharp',
-            'dart',
-            'docker',
-            'gradle',
-            'hcl',
-            'java',
-            'lua',
-            'php',
-            'properties',
-            'r',
-            'ruby',
-            'scala',
-            'swift',
-            'brightscript',
-            'rust',
-            'toml',
-            'django',
-            'yaml',
-            'kotlin',
-          ],
-        },
-        algolia: {
-          appId: '9HS3C2MKTH',
-          apiKey: 'f22e24c1b333034a75914759b0f045c3',
-          indexName: 'snowplow',
-          contextualSearch: true,
-        },
-      }),
-  }
+          {
+            href: 'https://github.com/snowplow/',
+            label: 'GitHub',
+            position: 'left',
+          },
+          {
+            to: 'https://try.snowplowanalytics.com/?utm_content=hero-cta&utm_campaign=snowplow-docs',
+            label: 'Try Snowplow',
+            className: 'snwpl-nav-button',
+            position: 'right',
+          },
+          {
+            to: 'https://go.snowplowanalytics.com/l/571483/2021-02-19/3sn5nml',
+            label: 'Book a demo',
+            className: 'snwpl-nav-button',
+            position: 'right',
+          },
+          {
+            type: 'custom-liveSnippetNavbarItem',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            label: 'Change cookie preferences',
+            href: '/cookie-preferences',
+          },
+          {
+            label: 'Terms and conditions',
+            href: '/terms-and-conditions',
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Snowplow Analytics Ltd. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: require('prism-react-renderer/themes/shadesOfPurple'),
+        // Docusaurus comes with a subset of commonly used languages -https://github.com/FormidableLabs/prism-react-renderer/blob/master/src/vendor/prism/includeLangs.js.
+        // To add syntax highlighting for additional Prism supported languages, add reference from https://prismjs.com/#supported-languages.
+        // NOTE: do a `yarn build` to ensure that it does build properly
+        additionalLanguages: [
+          'arduino',
+          'csharp',
+          'dart',
+          'docker',
+          'gradle',
+          'hcl',
+          'java',
+          'lua',
+          'php',
+          'properties',
+          'r',
+          'ruby',
+          'scala',
+          'swift',
+          'brightscript',
+          'rust',
+          'toml',
+          'django',
+          'yaml',
+          'kotlin',
+        ],
+      },
+      algolia: {
+        appId: '9HS3C2MKTH',
+        apiKey: 'f22e24c1b333034a75914759b0f045c3',
+        indexName: 'snowplow',
+        contextualSearch: true,
+      },
+    }),
 }
